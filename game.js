7a078e7e7f050806416b0a354eb5f174a2d849ad
jayyp35 <jprabhu35@gmail.com>
2021-03-02 13:45:25 +0530

    Added images and made the wheel rotating

--- game.js
+++ game.js
@@ -14,13 +14,34 @@ let game = new Phaser.Game(config);
 
 function preload() {
     console.log("In preload");
+    //loading images using load object of 'this'
+    this.load.image('background','./resources/bg.jpg')
+    console.log(this);
+    this.load.image('wheel','./resources/wheel2.png')
+    this.load.image('pin','./resources/pin.png')
+    this.load.image('stand','./resources/stand.png')
 }
 
 function create() {
     console.log("Create");
+    let  w = game.config.width
+    let h = game.config.height
+    let background = this.add.sprite(w/2,h/2,'background')
+    //sprite is a phaser image
+    background.setScale(0.20)
+
+    //creating the obejects
+    this.wheel = this.add.sprite(w/2,h/2,'wheel')
+    this.wheel.setScale(0.12)
+    this.pin = this.add.sprite(w/2,h/10,'pin')
+    this.pin.setScale(0.1)
+    
+    this.stand = this.add.sprite(w/2,0.96*h,'stand')
+    this.stand.setScale(0.1)
 }
 
 function update() {
     console.log("Inside update");
+    this.wheel.angle += 2
 }
 
